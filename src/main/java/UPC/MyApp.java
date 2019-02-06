7712855a4d9ed0e7b3ca3c3b21285d1d817cbb06
Suresh <Smartsuresh.p84@gmail.com>
2019-02-06 13:36:30 -0500

    Created UPC project

---
+++ src/main/java/UPC/MyApp.java
@@ -0,0 +1,14 @@
+package JenkinsDemo;
+
+import org.joda.time.LocalTime;
+
+class MyApp{
+
+        public static void main(String...args){
+               LocalTime time = new LocalTime();
+               System.out.println("Local Time is :"+time);
+               System.out.println("********* Welcome to Jenkins Gradle! ************");
+
+                }
+
+}
