090c90d86394661f1c6be76078688384b0888fdd
Learning <smartsuresh.p84@gmail.com>
2019-02-07 12:19:51 -0500

    Update MyApp.java

--- src/main/java/UPC/MyApp.java
+++ src/main/java/UPC/MyApp.java
@@ -7,7 +7,7 @@ class MyApp{
         public static void main(String...args){
                LocalTime time = new LocalTime();
                System.out.println("Local Time is :"+time);
-               System.out.println("********* Welcome to Jenkins Gradle! ************");
+               System.out.println("********* Welcome to Gradle with webhook! ************");
 
                 }
 
